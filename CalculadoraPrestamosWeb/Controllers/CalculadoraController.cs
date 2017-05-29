bc09242148d8cae0da4d7c347e56b3cd6370faca
FaustoRod <faustoelnike@gmail.com>
2017-05-29 14:13:23 -0400

    First Commit
    Primer Commit

---
+++ CalculadoraPrestamosWeb/Controllers/CalculadoraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraPrestamosWeb.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
